90e1239da06050f993a113523cf482f809eb0cde
Omobolaji123 <wisdomomobolaji@gmail.com>
2021-03-06 14:15:40 +0100

    First commit

--- script/views/homeView.js
+++ script/views/homeView.js
@@ -1,7 +1,13 @@
 class HomeView{
+    // NAV POPUPS SELECTION ELEMENTS
     _navContainer = document.querySelector(".nav__list");
     _previewContainer = document.querySelector(".nav-preview");
     _popUps = this._previewContainer.children;
+    // MENU CLICK SELECTION ELEMENTS
+    _menu = document.querySelector(".nav__icon--menu");
+    _sideNav = document.querySelector(".side-nav");
+    _closeBtn = document.querySelector(".side-nav__close");
+    _overlay = document.querySelector(".overlay");
     addHandlerNavPopup(){
         this._navContainer.addEventListener('mouseover',(function(e){
             const target = e.target.closest(".nav__item");
@@ -26,6 +32,23 @@ class HomeView{
         })
 
     }
+
+    _toogleSideView(offsetPercent,overlayDisplay){
+        this._sideNav.style.transform = `translateX(${offsetPercent}%)`;
+        this._closeBtn.style.transform =  `translateX(${offsetPercent < 0 ? offsetPercent + 125 : offsetPercent}%)`;
+        this._overlay.style.display = overlayDisplay;
+    }
+
+    addHandlerSideView(){
+        this._menu.addEventListener("click", this._toogleSideView.bind(this,0,"block"));
+        this._closeBtn.addEventListener("click", this._toogleSideView.bind(this,-150,"none"));
+        this._overlay.addEventListener("click", this._toogleSideView.bind(this,-150,"none"));
+        window.addEventListener("keydown",(function(e){
+            if (e.key !== 'ESCAPE') return;
+            this._toogleSideView(-150,"none");
+        }.bind(this)))
+    }
+
 }
 
 export default new HomeView();
